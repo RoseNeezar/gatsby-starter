760754d26d6cd4bcd1eefcfb5652c7816e3d9995
Neezar <rforlyffe@gmaicl.com>
2020-04-25 17:15:36 +0800

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import React from "react"
-import { View, Text } from "react-native"
 
 const IndexPage: React.FC = () => {
   return (
     <>
-      <View style={{ backgroundColor: "green" }}>
-        <Text>Hello World</Text>
-      </View>
+      <div>
+        <h1>hello world</h1>
+      </div>
     </>
   )
 }
