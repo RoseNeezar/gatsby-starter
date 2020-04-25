b91a9ac87e0e2fb1a5b75c345ada3a7b399dd166
Neezar <rforlyffe@gmaicl.com>
2020-04-25 16:41:55 +0800

    add react-native-web

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <View style={{ backgroundColor: "green" }}>
+        <Text>Hello World</Text>
+      </View>
+    </>
+  )
+}
+
+export default IndexPage
