72cde1ffa629fde442cd61f6df2254f7cf7128b9
Neezar <rforlyffe@gmaicl.com>
2020-09-01 22:08:46 +0800

    .

--- src/App/Base/Header.ts
+++ src/App/Base/Header.ts
@@ -3,4 +3,5 @@ import styled from "styled-components"
 export const Header = styled.div`
   height: 50px;
   background-color: ${({ theme }) => theme.action};
+  color: ${({ theme }) => theme.reaction};
 `
