970dae1a10f13aadf14b38ca4447272a67278f43
Neezar <rforlyffe@gmaicl.com>
2020-09-26 11:58:01 +0800

    remove rootstore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import React, { useContext } from "react"
 import styled from "styled-components"
 import Layout from "../App/Layout/Layout"
-import { RootStoreContext } from "../App/Store/rootStore"
 import { observer } from "mobx-react-lite"
 import { Header } from "../App/Base/Header"
+import { themeStore } from "../App/Store/themeStore"
 
 const PageLayout = styled.div`
   background-color: ${({ theme }) => theme.primary};
 `
 
 const IndexPage: React.FC = () => {
-  const rootStore = useContext(RootStoreContext)
-  const { setToggleTheme, toggleTheme } = rootStore.themeStore
+  const { setToggleTheme, toggleTheme } = themeStore
 
   const HandleTheme = () => {
     setToggleTheme()
